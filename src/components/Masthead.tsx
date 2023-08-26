ee7c0f1cf722bc67eec113b830987886a7827032
zhangji <1453411403@qq.com>
2023-08-26 13:36:45 +0800

    fix: 响应式修复

--- src/components/Masthead.tsx
+++ src/components/Masthead.tsx
@@ -2,7 +2,7 @@ import Weatherbar from "./Weatherbar";
 
 export default function Masthead({ adcode }: { adcode: string }) {
     return (
-        <section className="flex-1 w-full flex justify-center bg-primary-img bg-cover bg-no-repeat max-md:bg-primaryImg">
+        <section className="flex-1 w-full flex max-md:justify-center bg-primary-img bg-cover bg-no-repeat max-md:bg-primaryImg">
             <div className='masthead'>
                 <Weatherbar adcode={adcode}/>
             </div>
--- src/components/Weatherbar.tsx
+++ src/components/Weatherbar.tsx
@@ -10,28 +10,13 @@ async function Weatherbar({ adcode }: { adcode: string }) {
         output: 'JSON'
     })
     const res = await getFetch<LiveResult>(urlOfLives) // { cache: 'no-store' }
-    // const res = {
-    //     lives: [
-    //         {
-    //             province: '浙江',
-    //             city: '西湖区',
-    //             adcode: '330106',
-    //             reporttime: '2023-08-23 21:32:27',
-    //             weather: '多云',
-    //             temperature: '28',
-    //             winddirection: '东',
-    //             windpower: '≤3',
-    //             humidity: '77',
-    //         }
-    //     ]
-    // }
     console.log(res);
     
     if (!res) return null
     const data = res.lives
 
     return (
-        <div className='absolute bg-transparent p-10 left-0 max-md:-translate-x-1/2 max-md:left-[50%]'>
+        <div className='md:absolute bg-transparent p-10 left-0'> {/* 废弃css: max-md:-translate-x-1/2 max-md:left-[50%] */}
             <div className='text-light-1'>
                 <h3 className='text-primary text-lg font-bold tracking-wider mb-5 cursor-default'>实时天气</h3>
 
@@ -43,36 +28,36 @@ async function Weatherbar({ adcode }: { adcode: string }) {
                                 <div className='text-light-1 col-span-2'>{item.city}</div>
                             </div>
                             <div className='text-gray-1 grid grid-cols-5 gap-5'>
-                                <text className='col-span-3'>天气</text>
-                                <text className='text-light-1 col-span-2'>{item.weather}</text>
+                                <p className='col-span-3'>天气</p>
+                                <p className='text-light-1 col-span-2'>{item.weather}</p>
                             </div>
                             <div className='text-gray-1 grid grid-cols-5 gap-5'>
                                 <div className='col-span-3 flex'>
                                     <Image src="/assets/temperature.svg" alt='humidity' width={16} height={16} className='mr-1' />
                                     <p>温度</p>
                                 </div>
-                                <text className='text-light-1 col-span-2 gap-5'>{`${item.temperature}°`}</text>
+                                <p className='text-light-1 col-span-2 gap-5'>{`${item.temperature}°`}</p>
                             </div>
                             <div className='text-gray-1 grid grid-cols-5 gap-5'>
                                 <div className='col-span-3 flex'>
                                     <Image src="/assets/winddirection.svg" alt='humidity' width={16} height={16} className='mr-1' />
                                     <p>风向</p>
                                 </div>
-                                <text className='text-light-1 col-span-2 gap-5'>{item.winddirection}</text>
+                                <p className='text-light-1 col-span-2 gap-5'>{item.winddirection}</p>
                             </div>
                             <div className='text-gray-1 grid grid-cols-5 gap-5'>
                                 <div className='col-span-3 flex'>
                                     <Image src="/assets/windpower.svg" alt='humidity' width={16} height={16} className='mr-1' />
                                     <p>风力</p>
                                 </div>
-                                <text className='text-light-1 col-span-2 gap-5'>{`${item.windpower}米/秒`}</text>
+                                <p className='text-light-1 col-span-2 gap-5'>{`${item.windpower}米/秒`}</p>
                             </div>
                             <div className='text-gray-1 grid grid-cols-5 gap-5'>
                                 <div className='col-span-3 flex'>
                                     <Image src="/assets/humidity.svg" alt='humidity' width={16} height={16} className='mr-1' />
                                     <p>空气湿度</p>
                                 </div>
-                                <text className='text-light-1 col-span-2 gap-5'>{`${item.humidity}%`}</text>
+                                <p className='text-light-1 col-span-2 gap-5'>{`${item.humidity}%`}</p>
                             </div>
                             <div className='text-gray-3 text-xs'>
                                 发布于 {item.reporttime}
